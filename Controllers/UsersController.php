ac8fa846a210ee39a6e0cac32cbde5dc963ca284
gaelle kerforne <stage.seeweb@gmail.com>
2021-04-04 23:30:17 +0200

    Creation d' un generateur de formulaire et de sa page login'

---
+++ Controllers/UsersController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+class UsersController extends Controller
+{
+    public function login()
+    {
+        //On instancie le formulaire
+        $form = new Form;
+
+        // On ajoute chacune des parties qui nous intéressent
+        $form->debutForm()
+            ->ajoutLabelFor('email', 'Email')
+            ->ajoutInput('email', 'email', ['id' => 'email', 'class' => 'form-control'])
+            ->ajoutLabelFor('password', 'Mot de passe')
+            ->ajoutInput('password', 'password', ['id' => 'password', 'class' => 'form-control'])
+            ->ajoutBouton('Me connecter', ['class' => 'btn btn-primary'])
+            ->finForm();
+
+        // On envoie le formulaire à la vue en utilisant notre méthode "create"
+        $this->render('admin/login', ['loginForm' => $form->create()]);
+    }
+}
