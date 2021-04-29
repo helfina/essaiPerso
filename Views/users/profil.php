d5323ced52ecc1e81900ceac0346dd2931b15f82
gaelle kerforne <stage.seeweb@gmail.com>
2021-04-30 00:07:18 +0200

    indentation code

--- Views/users/profil.php
+++ Views/users/profil.php
@@ -1,23 +1,22 @@
 <h1>profile</h1>
 <section style="display: flex; ">
 
+    <nav style="display: flex; flex-direction: column;">
+        <img src="" alt="avatar">
+        <ul style="display: flex; flex-direction: column">
+            <li>nom</li>
+            <li>prenom</li>
+            <li>role</li>
+        </ul>
 
-
-<nav style="display: flex; flex-direction: column;">
-    <img src="" alt="avatar">
-    <ul style="display: flex; flex-direction: column">
-        <li>nom</li>
-        <li>prenom</li>
-        <li>role</li>
+    </nav>
+    <ul style="display: flex; flex-direction: column; margin-left: 100px">
+        <li><a href="/avis">voir mes avis</a></li>
+        <li><a href="/avis/ajouter">ajouter un avis</a></li>
     </ul>
 
-</nav>
-<ul style="display: flex; flex-direction: column; margin-left: 100px">
-    <li><a href="/avis">voir mes avis</a></li>
-    <li><a href="/avis/ajouter">ajouter un avis</a></li>
-</ul>
-
 </section>
+
 
 
 
