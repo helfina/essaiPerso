31652fd6e3b36381bdb2ccd2adcb60b16bb501e6
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-17 15:23:49 +0200

    ajout cdn bootstrap pour switch dans l'espace  admin( gestions des avis)

--- Views/admin/avis.php
+++ Views/admin/avis.php
@@ -2,6 +2,7 @@
 //var_dump($avis);
 $avis;
 ?>
+<div class="table-responsive">
 <table class="table table-striped">
     <thead>
     <th>id</th>
@@ -17,16 +18,19 @@ $avis;
             <td><?= $avi->titre ?></td>
             <td><?= $avi->description ?></td>
             <td>
+
                 <div class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                    <label class="custom-control-label" for="customSwitch1"><?= $avi->actif ?></label>
+                    <input type="checkbox" class="custom-control-input" id="customSwitch1<?= $avi->id ?>" <?= $avi->actif ? 'checked' : ''?>>
+                    <label class="custom-control-label" for="customSwitch1<?= $avi->id ?>"></label>
                 </div>
             </td>
             <td>
-                <a href="" class="btn btn-warning">modifier</a>
+                <!-- !TODO  ajouter un scripts js demandant si l'on est sur de vouloir modifier-->
+                <a href="/avis/modifier/<?= $avi->id ?>" class="btn btn-warning">modifier</a>
                 <a href="" class="btn btn-danger">supprimer</a>
             </td>
         </tr>
     <?php endforeach; ?>
     </tbody>
 </table>
+</div>
