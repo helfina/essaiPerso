9adfe3e60d4a8a4b189d978f0c047103b38811c7
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-17 12:21:57 +0200

    ajout des logo et lien vers la creatrice des logos

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -1,4 +1,10 @@
 <footer>
+    <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
+        <img src="/public/images/certified_by_dyslexic.png" alt="logo_Certified_by_dyslexique"/>
+    </a>
+    <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
+        <img src="/public/images/certified_by_dysorthographique.png" alt="logo_Certified_by_dysorthographique"/>
+    </a>
 
 </footer>
 <!--js-->
