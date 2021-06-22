dabbfeabe213f65da68f154072ae794ce6282754
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-22 11:45:25 +0200

    ajout de liens

--- Views/admin/index.php
+++ Views/admin/index.php
@@ -1,3 +1,3 @@
 <h1>Espace admin</h1>
 <p><a href="/admin/avis">Gerez les avis</a></p>
-<p><a href="/admin/users">Gerez les utilisateurs</a></p>
+<p><a href="/admin/listUser">Gerez les utilisateurs</a></p>
