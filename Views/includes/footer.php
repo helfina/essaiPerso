9dce546364df962bdbcb2a5a3bd898887794c794
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-26 18:36:16 +0200

    ajout de style et resolution du probleme de liens d'image

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -1,13 +1,16 @@
 <footer>
     <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
-        <img src="/public/images/certified_by_dyslexic.png" alt="logo_Certified_by_dyslexique"/>
+        <img src="/images/certified_by_dyslexic.png" alt="logo_Certified_by_dyslexique"/>
     </a>
     <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
-        <img src="/public/images/certified_by_dysorthographique.png" alt="logo_Certified_by_dysorthographique"/>
+        <img src="/images/certified_by_dysorthographique.png" alt="logo_Certified_by_dysorthographique"/>
     </a>
 
 </footer>
 <!--js-->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 <script src="/js/navbar.js"></script>
 <script src="/js/switche.js"></script>
+
 
