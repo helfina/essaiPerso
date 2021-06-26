3d6a7725f2037774cf905f0c782f2c896bc6c8d7
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-26 16:44:56 +0200

    ajout style btsp

--- Views/avis/lister.php
+++ Views/avis/lister.php
@@ -1,12 +1,14 @@
+<section class="d-flex flex-wrap">
 <?php
 //var_dump($_SESSION['user']['id']);
 foreach ($avis as $avi) : ?>
-    <article>
-        <a href="/avis/lire/<?= $avi->id ?>"><?= $avi->titre ?></a>
-        <p>
-            <span><?= $avi->created_at ?></span>
-            <?= $avi->description ?>
-        </p>
+    <article class="card border border-info rounded m-2 p-2">
+        <div class="d-flex">
+            <h3 class="mr-auto p-2"><?= $avi->titre ?></h3>
+            <span class="p-2"><?= $avi->created_at ?></span></br>
+        </div>
+        <p class="card-body"><?= $avi->description ?></p>
+          <a href="/avis/lire/<?= $avi->id ?>" class="btn btn-info">Read</a>
         <?php
             // si la session n'est pas vide et que la session est egale a l'user_id alors tu affiche le btn modifier
             if (!empty($_SESSION['user']['id']) && $_SESSION['user']['id'] == $avi->users_id ) :
@@ -17,3 +19,4 @@ foreach ($avis as $avi) : ?>
 
     </article>
 <?php endforeach; ?>
+</section>
