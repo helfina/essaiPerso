7719dd47dba72ed1de5acaed40cff424abe3c2a9
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-26 19:55:51 +0200

    modif de style

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -1,11 +1,14 @@
-<footer>
-    <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
-        <img src="/images/certified_by_dyslexic.png" alt="logo_Certified_by_dyslexique"/>
-    </a>
-    <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
-        <img src="/images/certified_by_dysorthographique.png" alt="logo_Certified_by_dysorthographique"/>
-    </a>
-<p>© 2021 G.Kerforne</p>
+<footer class="row">
+    <aside class="col-12">
+        <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
+            <img src="/images/certified_by_dyslexic.png" alt="logo_Certified_by_dyslexique" class="img-fluid rounded-circle"/>
+        </a>
+        <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
+            <img src="/images/certified_by_dysorthographique.png" alt="logo_Certified_by_dysorthographique" class="img-fluid rounded-circle"/>
+        </a>
+    </aside>
+
+<p class="col-12 text-center">© 2021 G.Kerforne</p>
 </footer>
 <!--js-->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
