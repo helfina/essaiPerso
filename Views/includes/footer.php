43b6bbb16532008873785f4fd1300418c3a44e7d
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-26 18:45:59 +0200

    ajout copyright

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -5,7 +5,7 @@
     <a href="https://globalgoodness.ca/un-logo-pour-dyslexiques/">
         <img src="/images/certified_by_dysorthographique.png" alt="logo_Certified_by_dysorthographique"/>
     </a>
-
+<p>© 2021 G.Kerforne</p>
 </footer>
 <!--js-->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
