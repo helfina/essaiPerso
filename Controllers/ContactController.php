f81915c2bddfffec0603100f8b393f9c16e04def
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-26 19:56:07 +0200

    modif de style

--- Controllers/ContactController.php
+++ Controllers/ContactController.php
@@ -14,17 +14,17 @@ class ContactController extends Controller
 
         // On ajoute chacune des parties qui nous intéressent
         $form->debutForm()
-            ->ajoutLabelFor('nom', 'nom')
-            ->ajoutInput('text', 'nom', ['id' => 'nom', 'class' => 'form-control'])
-            ->ajoutLabelFor('prenom', 'prenom')
-            ->ajoutInput('text', 'prenom', ['id' => 'prenom', 'class' => 'form-control'])
-            ->ajoutLabelFor('email', 'email')
-            ->ajoutInput('email', 'email', ['id' => 'email', 'class' => 'form-control'])
-            ->ajoutLabelFor('sujet', 'sujet')
-            ->ajoutSelect('text', ['Demande de devis', 'Renseignements', 'Prendre rdv', 'Autre' ], ['id' => 'sujet', 'class' => 'form-control'])
+            ->ajoutLabelFor('nom', 'Nom')
+            ->ajoutInput('text', 'nom', ['id' => 'nom', 'class' => 'form-control mb-2'])
+            ->ajoutLabelFor('prenom', 'Prénom')
+            ->ajoutInput('text', 'prenom', ['id' => 'prenom', 'class' => 'form-control mb-2'])
+            ->ajoutLabelFor('email', 'Email')
+            ->ajoutInput('email', 'email', ['id' => 'email', 'class' => 'form-control mb-2'])
+            ->ajoutLabelFor('sujet', 'Sujet')
+            ->ajoutSelect('text', ['Demande de devis', 'Renseignements', 'Prendre rdv', 'Autre' ], ['id' => 'sujet', 'class' => 'form-control mb-2'])
             ->ajoutLabelFor('message', 'Message')
-            ->ajoutTextarea('message', 'message', ['id' => 'message','class' => 'form-control'])
-            ->ajoutBouton('Envoyez', ['class' => 'btn btn-primary'])
+            ->ajoutTextarea('message', 'message', ['id' => 'message','class' => 'form-control mb-2'])
+            ->ajoutBouton('Envoyez', ['class' => 'btn btn-info'])
             ->finForm();
         // On envoie le formulaire à la vue en utilisant notre méthode "create"
         $this->render('front/contact', ['FormulaireDeContact' => $form->create()]);
