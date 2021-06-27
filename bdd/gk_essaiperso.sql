ceb4074a3fba771847862bc26fe038d01f3618b8
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-27 18:31:28 +0200

    modification de la bdd, ajout d'element manquant

--- bdd/gk_essaiperso.sql
+++ bdd/gk_essaiperso.sql
@@ -17,8 +17,15 @@ CREATE TABLE `avis` (
                         PRIMARY KEY (`id`),
                         KEY `users_id` (`users_id`),
                         CONSTRAINT `avis_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `avis` (`id`, `titre`, `description`, `created_at`, `actif`, `users_id`) VALUES
+(3,	'gaelle1',	'alban est  le  meilleure  codeur énorme',	'2021-04-27 09:50:25',	1,	7),
+(7,	'gaelle2',	'Gaelle  c\'est ma maman adorer.❤❤❤❤❤????????????????',	'2021-04-28 00:24:04',	1,	7),
+(8,	'test1',	'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	'2021-04-28 00:32:51',	1,	10),
+(9,	'test2',	'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	'2021-04-28 00:33:37',	1,	10),
+(10,	'bidule',	'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	'2021-04-28 00:34:12',	0,	8),
+(11,	'bidule1',	'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	'2021-04-28 00:34:26',	1,	8);
 
 CREATE TABLE `cv` (
                       `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,18 +40,22 @@ CREATE TABLE `cv` (
                       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
+INSERT INTO `cv` (`id`, `titre`, `ecole`, `nom_entreprise`, `lien`, `description`, `annee`, `duree`, `actif`) VALUES
+(2,	'Développeuse Intégratrice Web',	NULL,	'Seeweb',	'https://www.seeweb.fr/',	'Durant ce stage j\'ai utlisé les language et outils suivant :   Php , mySQL,',	'2020',	2,	1),
+(3,	'Développeuse Intégratrice Web',	NULL,	'Iole Solution',	'https://www.iole.fr/',	'Durant ce stage j\'ai utlisé les language et outils suivant :   Php(POO) , postgrSQL, Javascript(POO), ',	'2020',	2,	1),
+(4,	'Développeuse Intégratrice  En Réalisation d\'application Web',	'Greta Bretagne Sud',	'',	'https://www.grandeecolenumerique.fr/formations/apgen_069-greta-bretagne-sud-kercode',	'Formation KERCODE , language et outils appris : Javascript, Jquery, bootstrap, Wordpress, php, Laravel, MySQL, photoshop, Css, Html',	'2019',	7,	1),
+(5,	'Concepteur Développeur  D\'application Web',	'3W Academy',	'',	NULL,	'Durant cette periode j\'ai appris reactJs, je n\'ai paspu terminer la formation faute d\'avoir trouvre un contrat d\'apprentissage dans les temps donné mais cela ma permis d\'apprendre react',	'2020',	6,	1);
 
 CREATE TABLE `users` (
                          `id` int(11) NOT NULL AUTO_INCREMENT,
                          `email` varchar(255) NOT NULL,
                          `password` varchar(255) NOT NULL,
+                         `nom` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                         `prenom` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                          `roles` json DEFAULT NULL,
+                         `actif` tinyint(1) NOT NULL,
                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- 2021-06-17 09:41:40
-
--- 17/06/21 -
-ALTER TABLE `users`
-    ADD `actif` tinyint(1) NOT NULL;
+-- 2021-06-27 16:30:47
