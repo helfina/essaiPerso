a0558bbc2730d810af88a35462ac6d5159b1a3ab
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-27 17:22:02 +0200

    ajout d'une feuille de style pour la page profil

--- Views/users/profil.php
+++ Views/users/profil.php
@@ -1,8 +1,7 @@
-<h1>profile</h1>
-<section style="display: flex; ">
-
-    <nav style="display: flex; flex-direction: column;">
-        <img src="" alt="avatar">
+<section class="profil row">
+    <h1 class="col-12 mb-3">profile</h1>
+    <nav>
+        <img src="/images/avatar.png" alt="avatar">
         <ul style="display: flex; flex-direction: column">
             <li>nom</li>
             <li>prenom</li>
