d5b2daf0ca583b4ec5bc52ac5fa714e8366b81dc
gaelle kerforne <stage.seeweb@gmail.com>
2021-06-27 16:37:18 +0200

    ajout liens menu

--- Views/includes/header.php
+++ Views/includes/header.php
@@ -9,8 +9,9 @@
 
         <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Service</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/services">Service</a></li>
             <li><a href="/contact">Contact</a></li>
         </ul>
 
